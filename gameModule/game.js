a5b91a810126a5ea8a6cadb1b6ca8e11273a89c5
GautamSidtharth <sidtharn@thoughtworks.com>
2015-02-23 16:56:27 +0530

    Rearranged the file | put the code in the top | put the help content in the bottom

--- gameModule/game.js
+++ gameModule/game.js
@@ -1,12 +1,26 @@
 var ld = require('lodash');
-var gameUtil = {};
-
 var paths=[
 		[4,3,2,1,8,15,22,29,36,43,44,45,46,47,48,49,42,35,28,21,14,7,6,5,13,20,27,34,41,40,39,38,37,30,23,16,9,10,11,12,19,26,33,32,31,24,17,18,25],
 		[22,29,36,43,44,45,46,47,48,49,42,35,28,21,14,7,6,5,4,3,2,1,8,15,9,10,11,12,13,20,27,34,41,40,39,38,37,30,23,16,17,18,19,26,33,32,31,24,25],
 		[46,47,48,49,42,35,28,21,14,7,6,5,4,3,2,1,8,15,22,29,36,43,44,45,37,30,23,16,9,10,11,12,13,20,27,34,47,40,39,38,31,24,17,18,19,26,33,32,25],
 		[28,21,14,7,6,5,4,3,2,1,8,15,22,29,36,43,44,45,46,47,48,49,42,35,41,40,39,38,37,30,23,16,9,10,11,12,13,20,27,34,33,32,31,24,17,18,19,26,25]
 	];
+
+
+var gameUtil = {};
+gameUtil.thorowDice = function() {
+	var dice = [1,2,3,0];
+	return (ld.sample(dice)+ld.sample(dice) || 12);
+}
+gameUtil.thorowUntilFinish = function(){
+	var halts=[2,3,4];
+	var diceVal = thorowDice();
+	return halts.indexOf(diceVal)<0;
+}
+exports.gameUtil = gameUtil;
+
+
+// help content
 var game = {
 	who_sTurn:"player1",
 	player1 : {
@@ -35,18 +49,3 @@ var game = {
 		path:paths[2]
 	}
 };
-
-gameUtil.thorowDice = function() {
-	var dice = [1,2,3,0];
-	return (ld.sample(dice)+ld.sample(dice) || 12);
-}
-
-gameUtil.thorowUntilFinish = function(){
-	var halts=[2,3,4];
-	var diceVal = thorowDice();
-	return halts.indexOf(diceVal)<0;
-}
-
-
-exports.Game = game;
-exports.gameUtil = gameUtil;
