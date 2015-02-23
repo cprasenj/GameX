2820ce84c14b82354cf944414c7f054f0f37088b
GautamSidtharth <sidtharn@thoughtworks.com>
2015-02-23 18:57:18 +0530

    The sample content of mocha tests from adda project is removed

--- test/gameXTest.js
+++ test/gameXTest.js
@@ -4,26 +4,6 @@ var gameUtil = require("../gameModule/game.js").gameUtil;
 var GameX = require("../gameModule/game.js").GameX;
 var paths = require("../gameModule/paths.js").paths;
 
-// describe("Adda", function(){
-// 	beforeEach(function(){
-// 		fs.writeFileSync('./tests/data/instance.db',backUpDb);
-// 	});
-
-// 	describe("getMyTopics", function(){
-// 		it("should give all created and joined topics of mail mahesh@gmail.com", function(done){
-// 			records.getMyTopics("mahesh@gmail.com",function(err,myTopics){
-// 				assert.deepEqual(myTopics,[ 
-// 					{ id: 2, name: 'Music' },
-//   					{ id: 7, name: 'Vijayawada'},
-//   					{ id: 1, name: 'Cricket' },
-//   					{ id: 3, name: 'STEP' } ]);
-// 				done();
-// 			});
-// 		});
-// 	});
-
-
-
 describe("GameX ",function(){
 	beforeEach(function(){
 		game = new GameX(2);
