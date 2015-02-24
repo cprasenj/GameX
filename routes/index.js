29dae7b47b332a33314ce9aa7b7b90ebaad8f815
GautamSidtharth <sidtharn@thoughtworks.com>
2015-02-24 17:27:43 +0530

    moveCoin ajax function has abstract body`

--- routes/index.js
+++ routes/index.js
@@ -16,5 +16,15 @@ router.get('/updateDiceTable',function(req,res){
 		res.end(JSON.stringify(response));
 	});
 });
-
+router.get('/moveCoin',function(req,res){
+	var cId = req.query.cId;
+	var distance = req.query.distance;
+	Game.moveTo(Game.who_sTurn,cId,distance,function(player,cId){
+		var response = {
+			player: player,
+			cId: cId
+		};
+		res.end(JSON.stringify(response));
+	});
+});
 module.exports = router;
