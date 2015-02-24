c9d87d17d3032d48c229f63a66f54a2f0b332853
GautamSidtharth <sidtharn@thoughtworks.com>
2015-02-24 16:10:36 +0530

    Removed merge conflict messages from index.js

--- routes/index.js
+++ routes/index.js
@@ -9,16 +9,11 @@ router.get('/', function(req, res, next) {
 });
 router.get('/updateDiceTable',function(req,res){
 	Game.rollTheDice(function(newNumber){
-// <<<<<<< HEAD
-// 		res.end(newCount+'');
-// =======
-// 		// var newCountHtmlElement='<tr><td><input type="checkbox" name="diceVlaue" value="1"><td>'+newCount+'</td></tr>';
 		var response = {
 			"newNumber":newNumber,
 			"isFinished":!Game.hasToRollTheDiceAgain()
 		};
 		res.end(JSON.stringify(response));
-// >>>>>>> 3409398657a2c5d3ef3995ac9011f62defae6c8f
 	});
 });
 
