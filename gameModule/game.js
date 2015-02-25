8886ceeb5f7d1a6e58bd3444b71d45a8095be029
Prasenjit Chakraborty <cprasenj@thoughtworks.com>
2015-02-25 12:45:48 +0530

    player object created

--- gameModule/game.js
+++ gameModule/game.js
@@ -15,45 +15,51 @@ var samplePlayer = {
 	"isAssassin" : false
 };
 
+var players = function(numberOfPlayers) {
+	var players = {};
+	for(var i=1;i<=numberOfPlayers;i++){
+		players["player"+i] = JSON.parse(JSON.stringify(samplePlayer));
+		players["player"+i].path = paths[i-1];
+	};
+	return players;
+}
+
 var GameX = function(numberOfPlayers){
 	var game = {};
 	game.safeZones = [4,9,13,22,25,28,37,41,46];
 	game.who_sTurn = "player1";
-	game.players = ["player1","player2"];
-	for(var i=1;i<=numberOfPlayers;i++){
-		game["player"+i] = JSON.parse(JSON.stringify(samplePlayer));
-		game["player"+i].path = paths[i-1];
-	};
+	game.players  = new players(numberOfPlayers);
+	game.playerList = Object.keys(game.players);
 	game.moveTo = function(player,cId,distance,callAfterMoved){
-		game[player].coins[cId].position +=distance;
+		game.players[player].coins[cId].position +=distance;
 		callAfterMoved(player,cId);
 	};
 	game.hasToRollTheDiceAgain = function(){
 		var halts=[2,3,4];
 		var player=game.who_sTurn;
-		var diceVal = ld.last(game[player].diceRolled);
+		var diceVal = ld.last(game.players[player].diceRolled);
 		return !(halts.indexOf(diceVal)>=0);
 	};
 	game.rollTheDice = function(callback) {
 		diceval = giveDiceValue();
-		game[game.who_sTurn].diceRolled.push(diceval);
+		game.players[game.who_sTurn].diceRolled.push(diceval);
 		callback(diceval);
 	};
 	game.changePlayer = function() {
-		var turn = game.players.indexOf(game.who_sTurn);
-		turn == game.players.length - 1 ? (game.who_sTurn = game.players[0]) :
-		(game.who_sTurn = game.players[turn+1]); 
+		var turn = game.playerList.indexOf(game.who_sTurn);
+		turn == game.playerList.length - 1 ? (game.who_sTurn = game.playerList[0]) :
+		(game.who_sTurn = game.playerList[turn+1]); 
 	};
 	game.getAtHome = function() {
-		return game.players.map(function(player){
-			return game[player].coins.reduce(function(count,coin){
+		return game.playerList.map(function(player){
+			return game.players[player].coins.reduce(function(count,coin){
 				return coin.position == 49 ? (count + 1) : count;
 			},0);
 		})
 	};
 	game.getOnBoard = function(){
-		return game.players.map(function(player){
-			return game[player].coins.reduce(function(count,coin){
+		return game.playerList.map(function(player){
+			return game.players[player].coins.reduce(function(count,coin){
 				return coin.position && coin.position < 49 ? (count + 1) : count;
 			},0);
 		})	
